b1fd3519abcfcdd406553052b21ec3781f93eb6e
adeelfeb <at4563323@gmail.com>
2024-10-08 18:29:41 +0500

    don't know what it is asking for commiting but let it be

--- contextAPI/src/App.jsx
+++ contextAPI/src/App.jsx
@@ -7,10 +7,19 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+   
+            <div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        
+                    </div>
 
-      <h1>Hi there</h1>
-    </>
+                    <div className="w-full max-w-sm mx-auto">
+                       
+                    </div>
+                </div>
+            </div>
+
   )
 }
 
