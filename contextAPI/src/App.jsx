31929c5f2bb9925ba2279f6b8a81219cb496bf41
adeelfeb <at4563323@gmail.com>
2024-10-10 16:31:03 +0500

    Context API is really something that need to be understood more clearly

--- contextAPI/src/App.jsx
+++ contextAPI/src/App.jsx
@@ -2,23 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ThemeProvider } from './contexts/Theme'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    
-            <div className="flex flex-wrap min-h-screen items-center bg-black">
+            <ThemeProvider value={{DarkMode, LightMode, themeMode}}>
+            <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
-                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4 text-white text-4xl">
-                        he
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        {/* Theme button */}
                     </div>
 
                     <div className="w-full max-w-sm mx-auto">
-                       
+                       {/* Cards */}
                     </div>
                 </div>
             </div>
+            </ThemeProvider>
 
   )
 }
