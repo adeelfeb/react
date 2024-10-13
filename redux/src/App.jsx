6e700451924540dcf8c9a78bbdc8343165b378db
adeelfeb <at4563323@gmail.com>
2024-10-13 15:58:49 +0500

    Todo For redux tool kit understanding

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <h1 className='p-5 bg-red-400 mx-8 my-8'>Hi there </h1>
+    < >
+    <div className='p-8'>
+    <h1 className='p-5 bg-blue-400 mx-8 my-8 rounded-lg text-bold text-4xl text-center'>Todo Project <br /> (for Redux-ToolKit understanding!)</h1>
+    <Todos/>
+    <AddTodo/>
+    </div>
     </>
   )
 }
