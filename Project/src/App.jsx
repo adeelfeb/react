a75804143ca04358e82748578b6b7e429a72d3d6
adeelfeb <at4563323@gmail.com>
2024-10-18 15:48:59 +0500

    Environment variables are all setup and backend as a service has been utilized

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
+import config from './config/config';
+
+
 
 function App() {
   const [count, setCount] = useState(0);
+  console.log(config.appwriteCollectionId)
 
   return (
     <>
-      <h1 className="font-bold">This is just !!@</h1>
+      <h1 className="font-bold">This is just !!@{config.appwriteBucketId} </h1>
+      
     </>
   );
 }
