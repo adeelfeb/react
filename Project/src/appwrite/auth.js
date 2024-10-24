eaeb0908821f14af6cafd0b85a1a1eb3e3fd7b27
adeelfeb <at4563323@gmail.com>
2024-10-24 17:41:50 +0500

    Appwrite backend connection for account
    Create account
    Login
    Logout
    GetUser if any

---
+++ Project/src/appwrite/auth.js
@@ -0,0 +1,65 @@
+import conf from "../conf/conf";
+
+import {Client, Account , ID} from "appwrite"
+
+class AuthService{
+    client = new Client()
+    account;
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId)
+
+        this.account = new Account(this.client)
+    }
+
+    async creatAccount({name, email, password}){
+        try {
+            const userAccount = await this.account.create(
+                ID.unique(),
+                email,
+                password,
+                name,
+            )
+            if(userAccount){
+                //Login in the user when account is succefully created
+                return this.login({email, password})
+            }
+            else{
+                return userAccount
+            }
+        } catch (error) {
+            console.log("Error while creating user account:::", error)
+        }
+    }
+
+    async login({email, password}){
+        try {
+            return await this.account.createEmailSession(email, password)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get()
+        } catch (error) {
+            throw error
+        }
+        return null
+    }
+
+    async logout(){
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            throw error
+        }
+    }
+}
+
+
+const authService = new AuthService()
+
+export default authservice
