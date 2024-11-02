7e26099a094ed3786988b0e1e85cca2c290e7527
adeelfeb <at4563323@gmail.com>
2024-11-02 12:25:06 +0500

    Check Point: Logout component btn

---
+++ Project/src/components/Header/LogOutBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import authService from '../../appwrite/auth'
+
+function LogOutBtn() {
+  return (
+    <div>LogOutBtn</div>
+  )
+}
+
+export default LogOutBtn
