9b2e14bfd84d2290757645c7bb19670ac836aee2
adeelfeb <at4563323@gmail.com>
2024-11-03 14:01:55 +0500

    Created a Button component to be used
    things start making sense after spending time on them

--- Project/src/components/Footer/Logo.jsx
+++ Project/src/components/Footer/Logo.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 function Logo() {
   return (
-    <div>Logo</div>
-  )
+    <div>
+      <img
+        src="https://via.placeholder.com/50?text=Watch+Post"
+        alt="Amaze Post"
+        style={{ width: '45px', height: 'auto' }}
+      />
+    </div>
+  );
 }
 
-export default Logo
+export default Logo;
--- Project/src/components/Header/Header.jsx
+++ Project/src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ import { useNavigate } from 'react-router-dom'
 function Header() {
   const authStatus = useSelector((state) =>state.auth.status)
   const navigate = useNavigate()
-  const navItem = [
+  const navItems = [
     {
       name: 'Home',
       slug: "/",
@@ -41,11 +41,34 @@ function Header() {
   return (
     <header className='py-4 shadow bg-gray-500 '>
       <Container>
-        <nav></nav>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width="50px"></Logo>
+            </Link>
+          </div>
+          <ul className='flex ml-auto'>
+              {navItems.map((item)=> 
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                  onClick={()=>navigate(item.slug)}
+                  className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                  >{item.name}</button>
+                </li>
+              ) : null
+              )}
+              {authStatus && (
+                <li>
+                  <LogOutBtn/> 
+                </li>
+              )}
+          </ul>
+        </nav>
       </Container>
     </header>
   )
 }
-import { formatProdErrorMessage } from '@reduxjs/toolkit'
+
 
 export default Header
