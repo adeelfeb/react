1a7b43a45423b429d3ffaad55df4f4e753291630
adeelfeb <at4563323@gmail.com>
2024-11-03 12:04:51 +0500

    Issue was with BrowserProvider need to include that also in main.jsx
    Sometime need to remember seeing the bigger
    picture

--- Project/src/components/index.js
+++ Project/src/components/index.js
@@ -1,7 +1,15 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from './container/Container';
+import Logo from './Footer/Logo'
+import LogOutBtn from "./Header/LogOutBtn";
+
+
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogOutBtn
 }
--- Project/src/main.jsx
+++ Project/src/main.jsx
@@ -1,14 +1,36 @@
-// index.js
+// // index.js
+
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root') // Ensure this ID matches your HTML file
+// );
+
+
+//----------------Now there is the routing issue here
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import store from './store/store';// Ensure this path is correct
+// import App from './App';
+// import './index.css'; //
+
+// main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store/store';// Ensure this path is correct
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import store from './store/store';
 import App from './App';
-import './index.css'; //
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter> {/* Wrap the app in BrowserRouter */}
+      <App />
+    </BrowserRouter>
   </Provider>,
-  document.getElementById('root') // Ensure this ID matches your HTML file
+  document.getElementById('root')
 );
