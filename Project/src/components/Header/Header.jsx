a1325ab4ae2b367240e40a798a513ca59f3bb6c0
adeelfeb <at4563323@gmail.com>
2024-11-07 11:43:23 +0500

    Input component with forwardRed integration

--- Project/src/components/Header/Header.jsx
+++ Project/src/components/Header/Header.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import {Container, Logo, LogOutBtn} from '../index'
 import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import {useSelector} from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
-
-
-
 function Header() {
-  const authStatus = useSelector((state) =>state.auth.status)
+  const authStatus = useSelector((state) => state.auth.status)
   const navigate = useNavigate()
-  const navItem = [
+
+  const navItems = [
     {
       name: 'Home',
       slug: "/",
@@ -38,14 +36,38 @@ function Header() {
   },
   ]
 
+
   return (
-    <header className='py-4 shadow bg-gray-500 '>
+    <header className='py-3 shadow bg-gray-500'>
       <Container>
-        <nav></nav>
-      </Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width='70px'   />
+
+              </Link>
+          </div>
+          <ul className='flex ml-auto'>
+            {navItems.map((item) => 
+            item.active ? (
+              <li key={item.name}>
+                <button
+                onClick={() => navigate(item.slug)}
+                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                >{item.name}</button>
+              </li>
+            ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogOutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+        </Container>
     </header>
   )
 }
-import { formatProdErrorMessage } from '@reduxjs/toolkit'
 
 export default Header
